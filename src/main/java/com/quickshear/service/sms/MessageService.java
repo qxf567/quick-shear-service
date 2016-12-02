59b33864ab7870f7cfa6c74130bb8c9a3d350a53
qxf567 <qxf567@163.com>
2016-12-02 09:34:39 +0800

    测试不发短信

--- src/main/java/com/quickshear/service/sms/MessageService.java
+++ src/main/java/com/quickshear/service/sms/MessageService.java
@@ -7,43 +7,36 @@ import org.springframework.stereotype.Service;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
 import com.aliyuncs.exceptions.ClientException;
-import com.aliyuncs.exceptions.ServerException;
 import com.aliyuncs.profile.DefaultProfile;
 import com.aliyuncs.profile.IClientProfile;
 import com.aliyuncs.sms.model.v20160927.SingleSendSmsRequest;
-import com.aliyuncs.sms.model.v20160927.SingleSendSmsResponse;
 
 /**
  * 发短信服务
  * 
- * https://help.aliyun.com/document_detail/29459.html?spm=5176.doc35376.6.588.aNb4tf
+ * https://help.aliyun.com/document_detail/29459.html?spm=5176.doc35376.6.588.
+ * aNb4tf
  */
 @Service
 public class MessageService {
 
     /**
      * 发信短信验证码,返回四位验证码
+     * @throws ClientException 
      */
-    public String sendRandomCode(String phone) {
+    public String sendRandomCode(String phone) throws ClientException {
 	String code = null;
-	try {
-	    IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI2ySw5qXKk9C1",
-		    "BUu7ifM10cQeth6mhMJ8PdVXpllDsb");
-	    DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", "Sms", "sms.aliyuncs.com");
-	    IAcsClient client = new DefaultAcsClient(profile);
-	    SingleSendSmsRequest request = new SingleSendSmsRequest();
-	    request.setSignName("仟丝顺");
-	    request.setTemplateCode("SMS_32505240");
-	    code = getRandomCode(4);
-	    request.setParamString("{\"code\":\"" + code + "\"}");
-	    request.setRecNum(phone);
-	    SingleSendSmsResponse httpResponse = client.getAcsResponse(request);
-	    System.out.println(httpResponse);
-	} catch (ServerException e) {
-	    e.printStackTrace();
-	} catch (ClientException e) {
-	    e.printStackTrace();
-	}
+	IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI2ySw5qXKk9C1", "BUu7ifM10cQeth6mhMJ8PdVXpllDsb");
+	DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", "Sms", "sms.aliyuncs.com");
+	IAcsClient client = new DefaultAcsClient(profile);
+	SingleSendSmsRequest request = new SingleSendSmsRequest();
+	request.setSignName("仟丝顺");
+	request.setTemplateCode("SMS_32505240");
+	code = getRandomCode(4);
+	request.setParamString("{\"code\":\"" + code + "\"}");
+	request.setRecNum(phone);
+	//@TODO测试期间不发短信
+	//client.getAcsResponse(request);
 	return code;
     }
 
