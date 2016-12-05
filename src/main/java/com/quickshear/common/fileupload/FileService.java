1c17bf76a606d7c2ae4dd7e585632c95721a48fe
denglongshan <denglongshan@lashou-inc.com>
2016-12-05 16:28:55 +0800

    init

--- src/main/java/com/quickshear/common/fileupload/FileService.java
+++ src/main/java/com/quickshear/common/fileupload/FileService.java
@@ -1,7 +1,9 @@
 package com.quickshear.common.fileupload;
 
-import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,24 +35,43 @@ public class FileService {
 	 *            是否加水印
 	 * @return
 	 */
-	public String uploadSinglePic(MultipartFile mf, boolean watermark) {
+	public String uploadSinglePic(MultipartFile mf, boolean watermark,
+			String folderName) {
 		String msg = "{\"success\":%b, \"filePath\":\"%s\", \"message\":\"%s\"}";
-		String fileName = mf.getOriginalFilename().replace(",", "_")
-				.replace("&", "_");
+		String fileExtName = getFileExtName(mf);
 		try {
 			// 自定义的文件名称
 			String trueFileName = String.valueOf(System.currentTimeMillis())
-					+ fileName;
+					+ "."+fileExtName;
 			// 设置存放图片文件的路径
-			String path = config.getFastdfsServerHost()+trueFileName;
-			System.out.println("存放图片文件的路径:" + path);
-			// 转存文件到指定的路径
-			mf.transferTo(new File(path));
+			String path = config.getFastdfsServerHost()
+					+ System.getProperty("file.separator") + folderName
+					+ System.getProperty("file.separator") + trueFileName;
+
+			FileOutputStream fs = new FileOutputStream(path);
+			InputStream inputStream = mf.getInputStream();
+			byte[] buffer = new byte[1024 * 1024];
+			int byteread = 0;
+			while ((byteread = inputStream.read(buffer)) != -1) {
+				fs.write(buffer, 0, byteread);
+				fs.flush();
+			}
+			fs.close();
+			inputStream.close();
 			return String.format(msg, true, trueFileName, "上传成功");
 		} catch (Exception e) {
-			logger.error("Fail to upload file, filename is " + fileName, e);
+			logger.error("Fail to upload file", e);
 			return String.format(msg, null, null, "上传失败，请重试或与管理员联系");
 		}
+	}
+	
+	private String getFileExtName(MultipartFile mf) {
+		String filename = mf.getOriginalFilename();
+		if (StringUtils.isNotBlank(filename) && filename.contains(".") && !filename.endsWith("."))
+		{
+			return filename.substring(filename.lastIndexOf(".") + 1).toLowerCase();
+		}
+		return null;
 	}
 
 }
