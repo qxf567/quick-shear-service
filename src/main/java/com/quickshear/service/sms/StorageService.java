c67a6991373765f0e0385c143235731d668d0f8a
qianxiaofei <qianxiaofei@QIANXIAOFEI.lashou.local>
2017-02-08 16:42:52 +0800

    1

--- src/main/java/com/quickshear/service/sms/StorageService.java
+++ src/main/java/com/quickshear/service/sms/StorageService.java
@@ -4,34 +4,27 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 /** 利用本地文件实现记录openid读取的位置记录 */
 @Service
 public class StorageService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(StorageService.class);
-    
-    public void set(String value,HttpServletResponse response) {
-	
-	LOGGER.info("debug set cookie:"+value);
-	
-	Cookie cookie = new Cookie("openid", value);
+    public void set(String key,String value,HttpServletResponse response) {
+	Cookie cookie = new Cookie(key, value);
 	cookie.setMaxAge(60 * 60 * 24 * 30); // 设置生存期为1小时
 	cookie.setDomain("m.qiansishun.com"); // 子域，在这个子域下才可以访问该Cookie
 	response.addCookie(cookie);
     }
 
-    public String get(HttpServletRequest request) {
+    public String get(HttpServletRequest request,String key) {
 
 	Cookie[] cookies = request.getCookies();
-	if(cookies == null){
+	if(cookies == null || key == null){
 	    return null;
 	}
 	for(Cookie cookie:cookies){
-	    if("openid".equals(cookie.getName())){
+	    if(key.equals(cookie.getName())){
 		return cookie.getValue();
 	    }
 	}
